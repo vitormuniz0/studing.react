8936e24234cb0622ffa0e02b4887b13765bc0845
vitormuniz0 <vitormuniz311@gmail.com>
2024-09-30 21:45:59 -0300

    Vendo aulas basicas para avancar no curso do basico ao avancado

---
+++ dio/src/App.js
@@ -0,0 +1,14 @@
+import Button from "./components/button";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Olá React</h1>
+        <Button title="Click me"/>
+      </div>
+    </>
+  );
+}
+
+export default App;
